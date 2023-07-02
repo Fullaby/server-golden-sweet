a6869cf7c79be14759439c83238b1c4348718f8c
Steven Leonardo <stevenleonardolim@gmail.com>
2023-07-02 16:28:27 +0700

    read database

--- controllers/controllerData.js
+++ controllers/controllerData.js
@@ -1,9 +1,12 @@
+const {dataCollection}= require('../models');
+
 class ControllerData{
 static async getData(req,res){
     try {
-        
+        let data= await dataCollection.findAll()
+        res.status(200).json(data)
     } catch (error) {
-        
+        console.log(error)
     }
 }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router= require('express').Router();
 const Controller= require('../controllers/controllerData');
 
-router.use('/create', Controller.)
+router.use('/read', Controller.getData)
 
 module.exports= router
